656dd2952b700f1167f2e7f3efec9e808c722aae
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-02-02 10:45:24 +0100

    02022024

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,36 +3,44 @@ import _throttle from 'lodash.throttle';
 // стала для зберігання ключа keyStorage
 
 const keyStorage = 'feedback-form-state';
-const form = document.querySelector('.feedback-form');
 
+const form = document.querySelector('.feedback-form');
 
 // оператор, який буде справджати, чи є збережені дані користувача
 // якщо ні, то пусте
 
-form.elements.message.value = localStorage.getItem(keyStorage) || "";
+let save = {};
+if(localStorage.getItem(keyStorage)) {
+  let saveLocal = JSON.parse(localStorage.getItem(keyStorage));
+
+  if(saveLocal.email) form.email.value = saveLocal.email;
+  if(saveLocal.message) form.message.value = saveLocal.message;
+}
 
 // функція, яка буде зберігати дані в сховищі
-
-const saveData = (e) => {
-    localStorage.setItem(keyStorage, e.target.value);
-}
-// після висилання форми вичистити поле сховище, видати в консоль результат
-
-form.addEventListener("input", (e) => {
-  localStorage.setItem(keyStorage, e.target.value);
-});
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  localStorage.removeItem(keyStorage);
-  form.reset();
-});
 // сховище має оновлюватись не частіше 0,5 секунди
 
-const throttleSaveData = _throttle(saveData, 500);
+form.addEventListener('input', _throttle((event) => {
+  if(localStorage.getItem(keyStorage)) save = JSON.parse(localStorage.getItem(keyStorage));
 
-//  обробник події input, який використовує створену функцію
-form.addEventListener("input", throttleSaveData);
+  save[event.target.value] = event.target.value;
+  localStorage.setItem(keyStorage, JSON.stringify(save));
+}, 500));
+
+// щоб не перезавантажувалась автоматично сторінка, використано preventDefault
+// після висилання форми вичистити поле сховище
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
 
+if(form.email.value && form.message.value) {
+  form.email.value = "";
+  form.message.value = "";
+  console.log(JSON.parse(localStorage.getItem(keyStorage)));
+  localStorage.clear();
+  save = {}; 
+}
 
-console.log(form);
+});
+
